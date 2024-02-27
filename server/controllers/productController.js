8ed84c18566392df09cdf6f20309328e54e44891
MarufPulok <maruf12@student.sust.edu>
2024-02-27 18:34:19 +0600

    api building WIP

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,5 +1,21 @@
-exports.getAllProducts = (req, res) => {
-    res.status(200).json({
-        message: "Route is working fine"
-    })
-}
+const Product = require("../models/productModel");
+
+// create product -- /api/v1/product/new -- Admin
+exports.createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+
+  res.status(201).json({
+    success: true,
+    product,
+  });
+};
+
+// get all products -- /api/v1/products -- Public
+exports.getAllProducts = async (req, res, next) => {
+  const products = await Product.find();
+
+  res.status(200).json({
+    success: true,
+    products,
+  });
+};
