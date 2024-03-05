3879a94c88a374c4dc99eef06ed1527c4895ab97
MarufPulok <maruf12@student.sust.edu>
2024-03-05 17:10:13 +0600

    feat(api): product apis

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -19,3 +19,63 @@ exports.getAllProducts = async (req, res, next) => {
     products,
   });
 };
+
+// product details -- PUBLIC
+exports.getProductDetails = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+// update product -- ADMIN
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
+
+
+// delete product -- ADMIN
+exports.deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  await product.deleteOne();
+
+  res.status(200).json({
+    success: true,
+    message: "Product is deleted",
+  });
+}
